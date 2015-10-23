03d18ddb5ed85b48340ce1ff1e012ced7de927d4
afoat <aarron.foat@gmail.com>
2015-10-23 17:39:12 -0500

    Added a very basic trie. This is still a work in progress - but what is here does work, although the interface may change when I finalize this class.  Added in order to try to shrink the in memory rubiks cube pattern database while it is generating. Ended up being much faster than current solution, but so far appears to take up more memory. More research needed on this one!

---
+++ Libraries/Foat/Collections/Generic/Trie.IEnumerable.cs
@@ -0,0 +1,24 @@
+﻿namespace Foat.Collections.Generic
+{
+    using System;
+    using System.Collections.Generic;
+
+    public sealed partial class Trie<TKey, TValue> : IEnumerable<KeyValuePair<TKey[], TValue>>
+        where TKey : IEquatable<TKey>
+    {
+
+        public IEnumerator<KeyValuePair<TKey[], TValue>> GetEnumerator()
+        {
+            if (this.Head != null)
+            {
+                foreach (KeyValuePair<TKey[], TValue> keyValuePair in this.Head)
+                    yield return keyValuePair;
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Libraries/Foat/Collections/Generic/TrieNode.IEnumerable.cs
@@ -0,0 +1,57 @@
+﻿namespace Foat.Collections.Generic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal sealed partial class TrieNode<TKey, TValue> : IEnumerable<KeyValuePair<TKey[], TValue>>
+        where TKey : IEquatable<TKey>
+    {
+
+        public IEnumerator<KeyValuePair<TKey[], TValue>> GetEnumerator()
+        {
+            if (this.Alphabet != null)
+            {
+                foreach (TKey keyPart in this.Alphabet.Keys)
+                { 
+                    Stack<TKey> currentWord = new Stack<TKey>();
+                    currentWord.Push(keyPart);
+
+                    foreach (var key in PreOrder(currentWord, this.Alphabet[keyPart]))
+                    {
+                        yield return key;
+                    }
+                }
+            }
+
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public IEnumerable<KeyValuePair<TKey[], TValue>> PreOrder(Stack<TKey> currentWord, TrieNode<TKey, TValue> trieNode)
+        {
+            if (!trieNode.Value.Equals(default(TValue)))
+            {
+                yield return new KeyValuePair<TKey[], TValue>(currentWord.Reverse().ToArray(), trieNode.Value);
+            }
+
+            if (trieNode.Alphabet != null)
+            {
+                foreach (TKey keyPart in trieNode.Alphabet.Keys)
+                {
+                    currentWord.Push(keyPart);
+
+                    foreach (KeyValuePair<TKey[], TValue> keyValuePair in PreOrder(currentWord, trieNode.Alphabet[keyPart]))
+                    {
+                        yield return keyValuePair;
+                    }
+
+                    currentWord.Pop();
+                }
+            }
+        }
+    }
+}
