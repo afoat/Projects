6ba1a2ee601823d71186eee455c0c57167f64e56
afoat <aarron.foat@gmail.com>
2015-10-24 16:29:21 -0500

    Adding some comments on the ConcurrentMMFQueue

--- Libraries/Foat/Collections/Generic/MMF/ConcurrentMMFQueue.cs
+++ Libraries/Foat/Collections/Generic/MMF/ConcurrentMMFQueue.cs
@@ -3,6 +3,10 @@
     using System;
     using System.IO.MemoryMappedFiles;
 
+    /// <summary>
+    /// This is a memory mapped file based queue that can store huge numbers of items since its cached on disk.
+    /// This version cannot be persisted (yet).
+    /// </summary>
     public class ConcurrentMMFQueue : IDisposable
     {
         
@@ -23,6 +27,15 @@
 
         public long Count { get; private set; }
 
+        /// <summary>
+        /// Creates a new instance of the ConcurrentMMFQueue class.
+        /// </summary>
+        /// <param name="name">The name of the queue to be used. Two processes using ConcurrentMMFQueues
+        /// with the same name will share the same queue.</param>
+        /// <param name="bytesPerItem">The queue can deal with bytes in chucks you can read the bytes for
+        /// one item all at the same time.</param>
+        /// <param name="itemCapacity">The number of items that the queue will store.</param>
+        /// <param name="itemCacheCapacity">The number of items that the memory mapped view stream cach will contain.</param>
         public ConcurrentMMFQueue(string name, int bytesPerItem, int itemCapacity, int itemCacheCapacity)
         {
             if (name == null)
@@ -54,6 +67,11 @@
             this.TailCache = new MMFCache(this.BytesPerItem, this.ItemCacheCapacity, this.TailItemIndex, this.MemoryMappedFile, MemoryMappedFileAccess.Write);
         }
 
+        /// <summary>
+        /// This method attempts to enqueue the bytes that represent an item onto the queue.
+        /// </summary>
+        /// <param name="item">the bytes for the item that you want to queue.</param>
+        /// <returns>Returns true if the item was successfully enqueued, false if the queue is full.</returns>
         public bool TryEnqueue(byte[] item)
         {
             if (item == null)
@@ -83,6 +101,11 @@
             return true;
         }
 
+        /// <summary>
+        /// This method attempts to dequeue the bytes of an item.
+        /// </summary>
+        /// <param name="item">An output parameter that will contain the bytes of the item being dequeued.</param>
+        /// <returns>True if an item was successfully dequeued. False if the queue is empty.</returns>
         public bool TryDequeue(out byte[] item)
         {
             lock(Locker)
@@ -111,13 +134,19 @@
         }
 
         bool disposed = false;
+
+        /// <summary>
+        /// Disposes the managed memory file and any associated views.
+        /// </summary>
         public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
 
-        // Protected implementation of Dispose pattern.
+        /// <summary>
+        /// Disposes the managed memory file and any associated views.
+        /// </summary>
         protected virtual void Dispose(bool disposing)
         {
             if (disposed)
--- Libraries/Foat/Collections/Generic/MMF/MMFCache.cs
+++ Libraries/Foat/Collections/Generic/MMF/MMFCache.cs
@@ -3,17 +3,44 @@
     using System;
     using System.IO.MemoryMappedFiles;
 
+    /// <summary>
+    /// A helper class that helps keep track of a MemoryMappedViewStream ... where it starts where
+    /// it ends. It provides methods to read and write from the stream.
+    /// </summary>
     internal class MMFCache : IDisposable
     {
         private readonly int BytesPerItem;
         private readonly int ItemCacheCapactity;
         private readonly long TotalCachedBytes;
 
+        /// <summary>
+        /// The item based index within the MemoryMappedFile where the MMFCache starts
+        /// </summary>
         internal int StartIndex { get; private set; }
+
+        /// <summary>
+        /// The item based index within the MemoryMappedFile where the MMFCache ends
+        /// </summary>
         internal int EndIndex { get; private set; }
+
+        /// <summary>
+        /// The memory mapped file that is used to manage the queue
+        /// </summary>
         internal MemoryMappedFile MemoryMappedFile { get; private set; }
+
+        /// <summary>
+        /// The chunk of the queue that we are currently reading from or writing to
+        /// </summary>
         private MemoryMappedViewStream MemoryMappedView;
-        
+
+        /// <summary>
+        /// Constructs an instance of the MMFCache
+        /// </summary>
+        /// <param name="bytesPerItem">The number of bytes each item in the cache will have</param>
+        /// <param name="itemCacheCapacity">The number of items that the cache will contain</param>
+        /// <param name="startIndex">The item based index within the MemoryMappedFile where the MMFCache starts</param>
+        /// <param name="memoryMappedFile">The MemoryMappedFile that we are reading to and writing from</param>
+        /// <param name="memoryMappedFileAccess">The access parameters that we will use to open the MemoryMappedViewStream</param>
         public MMFCache(int bytesPerItem, int itemCacheCapacity, int startIndex, MemoryMappedFile memoryMappedFile, MemoryMappedFileAccess memoryMappedFileAccess)
         {
             this.StartIndex = startIndex;
@@ -26,6 +53,10 @@
             this.MemoryMappedView = this.MemoryMappedFile.CreateViewStream(this.StartIndex*this.BytesPerItem, this.TotalCachedBytes, memoryMappedFileAccess);
         }
 
+        /// <summary>
+        /// Writes a byte array to the current MemoryMappedView
+        /// </summary>
+        /// <param name="item">The bytes that will be written to the MemoryMappedView</param>
         public void Write(byte[] item)
         {
             if (item == null)
@@ -37,6 +68,10 @@
             this.MemoryMappedView.Write(item, 0, this.BytesPerItem);
         }
 
+        /// <summary>
+        /// Returns an array of bytes that were previously enqueued
+        /// </summary>
+        /// <returns>An array of bytes that were previously queued</returns>
         public byte[] Read()
         {
             byte[] bytes = new byte[this.BytesPerItem];
@@ -49,13 +84,18 @@
         #region IDisposable
 
         bool disposed = false;
+        /// <summary>
+        /// Releases the ManagedMemoryViewStream maintained by thre MMFCache
+        /// </summary>
         public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
 
-        // Protected implementation of Dispose pattern.
+        /// <summary>
+        /// Releases the ManagedMemoryViewStream maintained by thre MMFCache
+        /// </summary>
         protected virtual void Dispose(bool disposing)
         {
             if (disposed)
